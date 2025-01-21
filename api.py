fb47ec46751b623746db584a5498b953e51eb27c
DIG P2P - Bitcoin para Iniciantes <105225162+BitcoinMaranhao@users.noreply.github.com>
2025-01-20 23:09:29 -0300

    Add files via upload

--- api.py
+++ api.py
@@ -1,17 +1,23 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import Dict
 
 from main import var_valor
 
 app = FastAPI()
-
+# Definindo o modelo para o corpo da requisição
+class ValueRequest(BaseModel):
+    amountInCents: float
 
 @app.get("/V1/API/calc_btc", 
           tags=["BTC"],
           summary="Valor satoshis",
           description="Retorna dados de sats com base em um falor fiat")
-async def calcular_btc(value:float):
-    resultado = await var_valor(value)
+async def calcular_btc(value: ValueRequest):
+    # Acessando o valor do JSON enviado
+    resultado = await var_valor(value.amountInCents)
+    
     if resultado:
         return resultado
+    
     return {"error": "Erro ao calcular resposta BTC"}
