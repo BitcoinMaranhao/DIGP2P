9aedc8e0130261fb9bfc4341449f9de9f7107150
DIG P2P - Bitcoin para Iniciantes <105225162+BitcoinMaranhao@users.noreply.github.com>
2025-01-20 23:41:54 -0300

    Add files via upload

--- api.py
+++ api.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Dict
 
-from main import var_valor
+from main import var_valor,var_valor_usdt
 
 app = FastAPI()
 # Definindo o modelo para o corpo da requisição
@@ -16,6 +16,19 @@ class ValueRequest(BaseModel):
 async def calcular_btc(value: ValueRequest):
     # Acessando o valor do JSON enviado
     resultado = await var_valor(value.amountInCents)
+    
+    if resultado:
+        return resultado
+    
+    return {"error": "Erro ao calcular resposta BTC"}
+
+@app.get("/V1/API/calc_usdt", 
+          tags=["USDT"],
+          summary="Valor USDT",
+          description="Retorna dados de usdt com base em um falor fiat")
+async def calcular_usdt(value: ValueRequest):
+    # Acessando o valor do JSON enviado
+    resultado = await var_valor_usdt(value.amountInCents)
     
     if resultado:
         return resultado
