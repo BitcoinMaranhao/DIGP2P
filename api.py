4e35814a9657346f8f4691fb69d4630167c62ebe
{FranzoiDev} <{gfranzoi.barbosa@gmail.com}>
2025-04-12 22:11:32 -0300

    Alterar modelo de requisição para aceitar valores em reais com centavos e criar novo modelo para conversão em satoshis

--- api.py
+++ api.py
@@ -5,16 +5,19 @@ from main import buy_usdt, buy_btc, sell_btc, sell_usdt
 
 app = FastAPI()
 
-# Definindo o modelo para o corpo da requisição
 class ValueRequest(BaseModel):
-    amount: int  # Valor inteiro em reais (ex.: 100 para R$ 100,00)
+    amount: float
+
+# Modelo calc_sats
+class SatsValueRequest(BaseModel):
+    amount: float  # Valor em reais, aceita centavos (ex.: 100.50 para R$ 100,50)
 
 @app.post("/V1/API/calc_btc",
           tags=["BTC"],
           summary="Valor em BTC",
           description="Retorna dados de BTC com base em um valor fiat")
 async def calcular_btc(value: ValueRequest):
-    resultado = await buy_btc(value.amount)  # Valor já em reais
+    resultado = await buy_btc(value.amount)  # Valor em reais
     if resultado:
         return resultado
     return {"error": "Erro ao calcular resposta BTC"}
@@ -24,7 +27,7 @@ async def calcular_btc(value: ValueRequest):
           summary="Valor em USDT",
           description="Retorna dados de USDT com base em um valor fiat")
 async def calcular_usdt(value: ValueRequest):
-    resultado = await buy_usdt(value.amount)  # Valor já em reais
+    resultado = await buy_usdt(value.amount)  # Valor em reais
     if resultado:
         return resultado
     return {"error": "Erro ao calcular resposta USDT"}
@@ -34,7 +37,7 @@ async def calcular_usdt(value: ValueRequest):
           summary="Valor venda BTC",
           description="Retorna dados de venda de BTC com base em um valor fiat")
 async def calcular_sell_btc(value: ValueRequest):
-    resultado = await sell_btc(value.amount)  # Valor já em reais
+    resultado = await sell_btc(value.amount)  # Valor em reais
     if resultado:
         return resultado
     return {"error": "Erro ao calcular resposta BTC"}
@@ -44,7 +47,7 @@ async def calcular_sell_btc(value: ValueRequest):
           summary="Valor venda USDT",
           description="Retorna dados de venda de USDT com base em um valor fiat")
 async def calcular_sell_usdt(value: ValueRequest):
-    resultado = await sell_usdt(value.amount)  # Valor já em reais
+    resultado = await sell_usdt(value.amount)  # Valor em reais
     if resultado:
         return resultado
     return {"error": "Erro ao calcular resposta USDT"}
@@ -53,8 +56,8 @@ async def calcular_sell_usdt(value: ValueRequest):
           tags=["BTC"],
           summary="Valor em satoshis",
           description="Converte um valor em BRL para satoshis com base na cotação do BTC")
-async def calcular_sats(value: ValueRequest):
-    resultado = await buy_btc(value.amount)  # Valor já em reais
+async def calcular_sats(value: SatsValueRequest):
+    resultado = await buy_btc(value.amount)  # Valor em reais
     if "quantidade" in resultado:
         sats = int(resultado["quantidade"] * 100_000_000)  # Converte BTC para satoshis
         return {"satoshis": sats}
